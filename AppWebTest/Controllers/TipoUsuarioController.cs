6927aea53e7b21d11e06ed1c62923eda066dcc44
Jona <nathanojnabetse@hotmail.com>
2020-12-23 11:56:23 -0500

    filtrado con varios textbox en distintos campos en la vista tipoUsuario

---
+++ AppWebTest/Controllers/TipoUsuarioController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AppWebTest.Models;
+
+namespace AppWebTest.Controllers
+{
+    public class TipoUsuarioController : Controller
+    {
+        private TipoUsuarioCLS oTipoVal; //almacena el modelo que recibimos de la vista
+
+        private bool buscarTipoUsuario(TipoUsuarioCLS oTipoUsuarioCLS)
+        {
+            bool busquedaId = true;
+            bool busquedaNombre = true;
+            bool busquedaDescripcion =true;
+            
+            if(oTipoVal.iidtipousuario > 0)
+            {
+                busquedaId = oTipoUsuarioCLS.iidtipousuario.ToString().Contains(oTipoVal.iidtipousuario.ToString());
+            }
+
+            if(oTipoVal.nombre != null)
+            {
+                busquedaNombre= oTipoUsuarioCLS.nombre.ToString().Contains(oTipoVal.nombre);
+            }
+
+            if(oTipoVal.descripcion != null)
+            {
+                busquedaDescripcion = oTipoUsuarioCLS.descripcion.ToString().Contains(oTipoVal.descripcion);
+            }
+           
+            return (busquedaId && busquedaNombre && busquedaDescripcion);
+        }
+
+        // GET: TipoUsuario
+        public ActionResult Index(TipoUsuarioCLS oTipoUsuario)
+        {
+            oTipoVal = oTipoUsuario;
+            List<TipoUsuarioCLS> listaTipoUsuario = null;
+            //Variable 
+            List<TipoUsuarioCLS> listaFiltrado;
+            
+            using (var bd = new BDPasajeEntities())
+            {
+
+                listaTipoUsuario = (from tipoUsuario in bd.TipoUsuario
+                                    where tipoUsuario.BHABILITADO == 1
+                                    select new TipoUsuarioCLS
+                                    {
+                                        iidtipousuario = tipoUsuario.IIDTIPOUSUARIO,
+                                        nombre = tipoUsuario.NOMBRE,
+                                        descripcion = tipoUsuario.DESCRIPCION
+                                    }).ToList();
+
+                if(oTipoUsuario.iidtipousuario == 0 
+                    && oTipoUsuario.nombre==null 
+                    && oTipoUsuario.descripcion == null)
+                {
+                    listaFiltrado = listaTipoUsuario;
+                }
+                else
+                {
+                    Predicate<TipoUsuarioCLS> pred = new Predicate<TipoUsuarioCLS>(buscarTipoUsuario);
+                    listaFiltrado = listaTipoUsuario.FindAll(pred);//recorre cada elemento de la lista y se evalua por el predicado
+                }
+            }
+
+                return View(listaFiltrado);
+        }
+    }
+}
---
+++ AppWebTest/Models/TipoUsuarioCLS.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AppWebTest.Models
+{
+    public class TipoUsuarioCLS
+    {
+        [Display(Name ="Id tipo Usuario")]
+        public int iidtipousuario { get; set; }
+        
+        [Display(Name = "Nombre tipo Usuario")]
+        [Required]
+        [StringLength(150,ErrorMessage ="Longitud Maxima 150")]
+        public string nombre {get; set;}
+
+        [Display(Name = "Descripcion")]
+        [Required]
+        [StringLength(250, ErrorMessage = "Longitud Maxima 250")]
+        public string descripcion { get; set; }
+
+        public int bhabiliado { get; set; }
+
+    }
+}
